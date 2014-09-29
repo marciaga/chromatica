562659238aed178b83a88b09e4e46a1df228b07f
marciaga <mark.arciaga@gmail.com>
2014-09-29 14:07:36 -0700

    adjusted AR query for search results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 	def index
-	  @results = Result.where(:search_history_id == SearchHistory.where(:user_id == current_user)).paginate(:page => params[:page], :per_page => 21).order(id: :desc)
+	  #@results = Result.where(:search_history_id == SearchHistory.where(:user_id == current_user)).paginate(:page => params[:page], :per_page => 21).order(id: :desc)
+	 @results = Result.joins(:search_history).merge(SearchHistory.where(:user_id => current_user)).paginate(:page => params[:page], :per_page => 21).order(id: :desc)
 	 return @results
 	end
 end
