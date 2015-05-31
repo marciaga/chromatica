6742513e73aadffe9c462f7d427467403ec5d2cf
Mark Arciaga <marciaga@MarkBot.local>
2015-05-31 10:35:41 -0700

    Set config vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :spotify, ENV['client_id'], ENV['client_secret'], scope: 'playlist-read-private user-read-private user-read-email user-library-modify'
+	provider :spotify, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, scope: 'playlist-read-private user-read-private user-read-email user-library-modify'
 end
 
 
